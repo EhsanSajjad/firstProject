922a0822602e8be990bf816df1ea3857f066451d
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2020-08-12 08:26:46 +0600

    tuple and set is added

--- tuple.py
+++
@@ -1,8 +0,0 @@
-"""
-tuple is like list. but you can not change the value of it.
-fact: tuple is fatser than lsit.
-"""
-
-tup = (3,2,1,5,6)
-print(tup)
-print(tup[0])
---
+++ tuple_and_set.py
@@ -0,0 +1,25 @@
+# tuple
+"""
+tuple is like list. but you can not change the value of it.
+fact: tuple is fatser than lsit.
+"""
+
+tup = (3,2,1,5,6)
+print(tup)
+print(tup[0])
+
+# set
+"""
+remember:
+> square brackets [] is for list
+> round brackets () is for tuple
+> curly brackets {} is for set
+
+set does not follow teh sequence. (SET IS NOT FOR THE SEQUENCE)
+It is a concept of Hash, to improve the performance.
+"""
+
+s = { 34, 34, 43, 1, 5, 7, 100}
+print(s)
+
+
